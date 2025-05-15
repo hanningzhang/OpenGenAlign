c75f4073c19ef0be62a9f616453ffad4003e82af
hanningzhang <hanning.zhang@outlook.com>
2025-05-15 04:33:12 +0000

    add ood eval

---
+++ ood_eval/musique/gen.py
@@ -0,0 +1,83 @@
+import os
+import argparse
+from tqdm import tqdm
+import json
+from vllm import LLM, SamplingParams
+from datasets import load_dataset
+
+
+QA_prompt = '''Answer the following question and put the final short answer into \\boxed{{}}:
+{question}
+You should reason step by step to get the final answer. Bear in mind that your response should be strictly based on the following passages:
+{passages}
+When you respond, you should refer to the source of information.'''
+
+DATA_TO_TEXT_prompt = '''Instruction:
+Write an objective overview about the following local business based only on the provided structured data in the JSON format.
+You should include details and cover the information mentioned in the customers’ review. The overview should be 100 - 200
+words. Don’t make up information.
+Structured data:
+{json_data}
+Overview:'''
+
+Summarization_prompt = '''Summarize the following news within {word_num} words:
+{news}
+output:'''
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_name_or_path", type=str, default='meta-llama/Llama-3.1-8B-Instruct')  # model path
+    parser.add_argument("--tensor_parallel_size", type=int, default=1)  # tensor_parallel_size
+    parser.add_argument("--output_dir", type=str, default="data/llama31_musique_dev_n4_reason_seed2.json")  # output location
+    parser.add_argument("--random_seed", type=int, default=2)  # random seed
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
+    
+    dataset = load_dataset("dgslibisey/MuSiQue",split='validation')  
+    # with open(args.dataset,"r") as f:
+    #    dataset = json.load(f)[:2000]
+    
+    gen_dataset = []
+    for sample in dataset:
+        string = ""
+        paragraph = sample['paragraphs']
+        for s in paragraph:
+            string += s['title'] + "\n\n"
+            string += s['paragraph_text'] + "\n\n"
+        
+        prompt = QA_prompt.format(question=sample['question'],passages=string)
+        gen_dataset.append({"prompt":prompt,"answer":[sample['answer']] + sample['answer_aliases'],"reference":string})
+    
+    sampling_params = SamplingParams(n=1, temperature=1.0, top_p=1, max_tokens=4096, stop=[], seed=args.random_seed)
+    print('sampling =====', sampling_params)
+    llm = LLM(model=args.model_name_or_path,tensor_parallel_size=args.tensor_parallel_size, dtype = "float16",enforce_eager=True, gpu_memory_utilization=0.8,swap_space=128,trust_remote_code=True)
+    
+    #prompt_temp = 'BEGINNING OF CONVERSATION: USER: {input} ASSISTANT:'
+    # prompt_temp = "A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions.\n\nUSER: {input}\nAssistant:"
+    # prompt = [prompt_temp.format(input=i['prompt']) for i in gen_dataset]
+    # format_prompt = prompt
+    prompt = [{"role":"user","content":i['prompt']} for i in gen_dataset]
+    tokenizer = llm.get_tokenizer()
+
+    format_prompt = []
+    for i in prompt:
+        conversations = tokenizer.apply_chat_template(
+            [i],
+            tokenize=False,
+            add_generation_prompt=True
+        )
+        format_prompt.append(conversations)
+    print(format_prompt[0])
+    #format_prompt = llm.apply_chat_template(prompt, add_generation_prompt=True, tokenize=False)
+    store_data = []
+    completions = llm.generate(format_prompt, sampling_params)
+    for i,output in enumerate(completions):
+        prompt = output.prompt
+        generated_text = [output.outputs[i].text for i in range(len(output.outputs))]
+        answers = generated_text
+        store_data.append({"prompt":prompt,"answers":answers,"ground_truth":gen_dataset[i]['answer'],"reference":gen_dataset[i]['reference']})
+        
+    with open(args.output_dir,'w') as f:
+        json.dump(store_data,f,indent=4,ensure_ascii=False)
---
+++ ood_eval/musique/rm_select.py
@@ -0,0 +1,79 @@
+from dataclasses import dataclass, field
+from typing import Any, Dict, List, Optional, Union
+from tqdm import tqdm
+# import evaluate
+import numpy as np
+import json
+import torch
+import torch.nn as nn
+from datasets import load_dataset
+# from peft import LoraConfig, TaskType, get_peft_model
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    HfArgumentParser,
+    Trainer,
+    TrainingArguments,
+)
+
+QA_prompt = '''Answer the following question:
+{question}
+Bear in mind that your response should be strictly based on the following passages:
+{passages}
+When you response, you should always cite the source of information.'''
+
+def compute_loss(model, inputs, return_outputs=False):
+    with torch.no_grad():
+        #print(inputs['input_ids'].shape)
+        rewards = model(
+            input_ids=inputs["input_ids"], attention_mask=inputs["attention_mask"]
+        )[0]
+        return rewards[0]
+        # rewards = model(
+        #     input_ids=inputs["input_ids"], attention_mask=inputs["attention_mask"]
+        # ).logits[0][0]
+        # return rewards
+    
+path = "HanningZhang/Llama3.1-RAG-Reward"
+#path = "nicolinho/QRM-Llama3.1-8B-v2"
+model = AutoModelForSequenceClassification.from_pretrained(
+    path, 
+    num_labels=1, 
+    torch_dtype=torch.bfloat16, 
+    #use_flash_attention_2=True,
+).eval().to(0)
+tokenizer = AutoTokenizer.from_pretrained(path, use_fast = False)
+#tokenizer.model_max_length = 8192
+model.config.pad_token_id = tokenizer.pad_token_id
+tokenizer.truncation_side = "left"
+with open("data/llama31_musique_dev_n4_reason.json",'r') as f:
+    data = json.load(f)
+
+count = 0
+total = 0
+
+for sample in tqdm(data):
+    prompt = sample['prompt']
+    prompt = prompt.replace("<|im_start|>system\nYou are Qwen, created by Alibaba Cloud. You are a helpful assistant.<|im_end|>\n<|im_start|>user\n","")
+    prompt = prompt.replace("<|im_end|>\n<|im_start|>assistant\n","")
+    prompt = prompt.replace("<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nCutting Knowledge Date: December 2023\nToday Date: 26 Jul 2024\n\n<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n","")
+    prompt = prompt.replace("<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n","")
+    score_list = []
+    for ans in sample['answers'][:]:
+        tmp1 = [{"role":"user","content":prompt},
+                {"role":"assistant","content":ans}]
+        sample1 = {}
+        sample1['positive'] = tokenizer.apply_chat_template(
+            tmp1, tokenize=False, add_generation_prompt=False).replace(tokenizer.bos_token, "")
+        tokenized_pos2 = tokenizer(sample1['positive'], truncation=True,return_tensors='pt').to(0)
+        sample1["input_ids"] = tokenized_pos2["input_ids"]
+        sample1["attention_mask"] = tokenized_pos2["attention_mask"]
+        score_chosen = compute_loss(model,sample1)
+        score_list.append(score_chosen)
+        
+    idx = score_list.index(max(score_list))
+    sample['max_idx'] = idx
+    
+with open("data/llama31_musique_dev_n4_reason_reward_select.json",'w') as f:
+    json.dump(data,f,indent=4,ensure_ascii=False)
+
